3e71b2d4cc6d024b2f5f45ee94e1919de0687f0b
ScottDowne <scott.downe@gmail.com>
2017-11-30 01:31:54 -0500

    Fixing url

--- pages/fundraising-2016-page.js
+++ pages/fundraising-2016-page.js
@@ -2,7 +2,7 @@ class Fundraising2016Page extends React.Component {
 
   render() {
     return (
-      <Fundraising2016 icon="/assets/heart-snippet.gif">
+      <Fundraising2016 icon="../assets/heart-snippet.gif">
         <b>Dear Firefox users:</b> Mozilla puts the public good and user privacy before profit. If Firefox is useful to you, take one minute to support the non-profit behind it. <em>If everyone reading this donates $3, Mozilla’s fundraiser would be over within an hour.</em> <i>Thank you.</i>
       </Fundraising2016>
     );
