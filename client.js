0deffab24c3ed38c4dadc50d1c0a6c722202aa20
ScottDowne <scott.downe@gmail.com>
2018-02-09 16:09:52 -0500

    Adding 2017 snippets

--- client.js
+++ client.js
@@ -1,20 +1,30 @@
 let render = ReactDOM.render;
 let HashRouter = ReactRouterDOM.HashRouter;
 let Route = ReactRouterDOM.Route;
+let Switch = ReactRouterDOM.Switch;
 
 render((
   <HashRouter>
     <div>
-      <Route exact path="/" component={Home}/>
-      <Route path="/net-neutrality" component={NetNeutrality} />
-      <Route path="/firefox-retention" component={FirefoxRetention} />
-      <Route path="/fr-2015" component={Fundraising2015} />
-      <Route path="/fr-2015-spectacular-1" component={Fundraising2015Spectacular1} />
-      <Route path="/fr-2015-spectacular-2" component={Fundraising2015Spectacular2} />
-      <Route path="/fr-2016" component={Fundraising2016Page} />
-      <Route path="/fr-2016-spectacular-1" component={Fundraising2016Spectacular1} />
-      <Route path="/fr-2016-spectacular-2" component={Fundraising2016Spectacular2} />
-      <Route path="/mass-surveillance" component={MassSurveillance} />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/net-neutrality" component={NetNeutrality} />
+        <Route path="/firefox-retention" component={FirefoxRetention} />
+        <Route path="/fr-2015" component={Fundraising2015} />
+        <Route path="/fr-2015-spectacular-1" component={Fundraising2015Spectacular1} />
+        <Route path="/fr-2015-spectacular-2" component={Fundraising2015Spectacular2} />
+        <Route path="/fr-2016" component={Fundraising2016Page} />
+        <Route path="/fr-2016-spectacular-1" component={Fundraising2016Spectacular1} />
+        <Route path="/fr-2016-spectacular-2" component={Fundraising2016Spectacular2} />
+        <Route path="/mass-surveillance" component={MassSurveillance} />
+
+        <Route path="/fr-2017-1" push={false} component={() => window.location = "https://snippets-admin.us-west.moz.works/show/uuid/b07671de-e214-4810-a78e-0cda9ca415f6/"}/>
+        <Route path="/fr-2017-2" push={false} component={() => window.location = "https://snippets-admin.us-west.moz.works/show/uuid/ef7b6377-893f-4e6d-a955-b49b2c9c5d67/"}/>
+        <Route path="/fr-2017-3" push={false} component={() => window.location = "https://snippets-admin.us-west.moz.works/show/uuid/52d82f59-e396-4b5e-bca2-b722b3663404/"}/>
+        <Route path="/fr-2017-4" push={false} component={() => window.location = "https://snippets-admin.us-west.moz.works/show/uuid/29426321-6d93-4ba0-b314-d1920b7c58ca/"}/>
+        <Route path="/fr-2017-5" push={false} component={() => window.location = "https://snippets-admin.us-west.moz.works/show/uuid/a891ed85-1712-4283-a669-bbccc01b4b1f/"}/>
+        <Route path="/fr-2017-6" push={false} component={() => window.location = "https://snippets-admin.us-west.moz.works/show/uuid/05483172-c620-4ed2-b4b5-37403f60d851/"}/>
+      </Switch>
     </div>
   </HashRouter>
 ), document.getElementById('app'))
--- components/home.js
+++ components/home.js
@@ -3,15 +3,21 @@ class Home extends React.Component {
     return (
       <div className="home-page">
         <h1>Snippet examples</h1>
-        <a href="#net-neutrality">Net Neutrality</a><br/>
-        <a href="#firefox-retention">Firefox Retention</a><br/>
-        <a href="#fr-2015">Fundraising 2015</a><br/>
-        <a href="#fr-2015-spectacular-1">Fundraising 2015 Spectacular 1</a><br/>
-        <a href="#fr-2015-spectacular-2">Fundraising 2015 Spectacular 2</a><br/>
-        <a href="#fr-2016">Fundraising 2016</a><br/>
-        <a href="#fr-2016-spectacular-1">Fundraising 2016 Spectacular 1</a><br/>
-        <a href="#fr-2016-spectacular-2">Fundraising 2016 Spectacular 2</a><br/>
-        <a href="#mass-surveillance">Mass Surveillance</a>
+        <a href="#/net-neutrality">Net Neutrality</a><br/>
+        <a href="#/firefox-retention">Firefox Retention</a><br/>
+        <a href="#/fr-2015">Fundraising 2015</a><br/>
+        <a href="#/fr-2015-spectacular-1">Fundraising 2015 Spectacular 1</a><br/>
+        <a href="#/fr-2015-spectacular-2">Fundraising 2015 Spectacular 2</a><br/>
+        <a href="#/fr-2016">Fundraising 2016</a><br/>
+        <a href="#/fr-2016-spectacular-1">Fundraising 2016 Spectacular 1</a><br/>
+        <a href="#/fr-2016-spectacular-2">Fundraising 2016 Spectacular 2</a><br/>
+        <a href="#/fr-2017-1">Fundraising 2017 1</a><br/>
+        <a href="#/fr-2017-2">Fundraising 2017 2</a><br/>
+        <a href="#/fr-2017-3">Fundraising 2017 3</a><br/>
+        <a href="#/fr-2017-4">Fundraising 2017 4</a><br/>
+        <a href="#/fr-2017-5">Fundraising 2017 5</a><br/>
+        <a href="#/fr-2017-6">Fundraising 2017 6</a><br/>
+        <a href="#/mass-surveillance">Mass Surveillance</a>
       </div>
     );
   }
